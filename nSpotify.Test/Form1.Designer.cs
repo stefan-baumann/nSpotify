117e13a68e84a1cecd839008d7d081f50a9a574c
Stefan Baumann <stefanbaumannbinzen@hotmail.de>
2015-02-18 20:37:23 +0100

    Add Test Application
    nSpotify.Test

---
+++ nSpotify.Test/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace nSpotify.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fetch Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(382, 331);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(400, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start/Stop Events";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(400, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(244, 329);
+            this.listBox1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(313, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "!! Don\'t use both test functions at the same time !!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(656, 384);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ nSpotify.Test/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using nSpotify;
+
+namespace nSpotify.Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            this.eProvider = new EventProvider(Spotify.DataProviderInstance, 50, this);
+            Action<string, object> ReportUpdate = (prop, value) =>
+            {
+                this.listBox1.Items.Add(prop + " Changed: " + value.ToString());
+                this.listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            };
+
+            this.eProvider.TrackChanged += (s, arg) => ReportUpdate("Track", provider.CurrentStatus.Track.Name);
+            this.eProvider.VolumeChanged += (s, arg) => ReportUpdate("Volume", provider.CurrentStatus.Volume);
+            this.eProvider.PlayStateChanged += (s, arg) => ReportUpdate("Play State", provider.CurrentStatus.Playing ? "Playing" : "Paused");
+
+            this.Disposed += Form1_Disposed;
+        }
+
+        void Form1_Disposed(object sender, EventArgs e)
+        {
+            this.eProvider.Dispose();
+        }
+
+        private DataProvider provider = Spotify.DataProviderInstance;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = provider.UpdateStatus().ToPropertyString();
+            Application.DoEvents(); //Awful to look at but hey, it's just a test app
+            this.pictureBox1.Image = provider.CurrentStatus.Track.DownloadAlbumArt(Track.AlbumArtSize.Size160);
+        }
+
+        private EventProvider eProvider;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            eProvider.Enabled = !eProvider.Enabled;
+        }
+
+        
+    }
+
+    public static class Extensions
+    {
+        public static string ToPropertyString(this object arg)
+        {
+            return arg.ToPropertyString(0);
+        }
+
+        private static string ToPropertyString(this object arg, int level)
+        {
+            StringBuilder output = new StringBuilder();
+            foreach(PropertyInfo property in arg.GetType().GetProperties())
+            {
+                if (property.PropertyType.Assembly == typeof(nSpotify.Spotify).Assembly)
+                {
+                    output.AppendLine(property.GetValue(arg).ToPropertyString(level + 1));
+                }
+                else
+                {
+                    output.AppendLine(string.Format("{0}{1}: {2}", new string(' ', level * 2), property.Name, property.GetValue(arg)));
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
